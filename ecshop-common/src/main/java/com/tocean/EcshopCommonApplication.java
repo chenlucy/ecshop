e12f49f4dd684ba98de4239159ecbfcfdbd10d58
chenlu <chenl@tocean.com>
2018-07-28 18:02:03 +0800

    dd

---
+++ ecshop-common/src/main/java/com/tocean/EcshopCommonApplication.java
@@ -0,0 +1,12 @@
+package com.tocean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcshopCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcshopCommonApplication.class, args);
+	}
+}
---
+++ ecshop-gateway/src/main/java/com/toceansoft/EcshopGatewayApplication.java
@@ -0,0 +1,30 @@
+package com.toceansoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringCloudApplication
+@Configuration
+public class EcshopGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EcshopGatewayApplication.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setMaxAge(18000L);
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
